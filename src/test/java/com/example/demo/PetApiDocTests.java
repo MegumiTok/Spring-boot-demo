7cbc8447f155b2d117fc58864aa739362a9d2a3d
sleepyshowdy <hojo.miku@gmail.com>
2024-02-04 20:29:49 +0900

    v5 added an API for creating a pet

--- src/test/java/com/example/demo/PetApiDocTests.java
+++ src/test/java/com/example/demo/PetApiDocTests.java
@@ -2,18 +2,23 @@ package com.example.demo;
 
 import com.example.demo.pet.Pet;
 import com.example.demo.pet.Pets;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDate;
-import java.time.Month;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -26,6 +31,9 @@ public class PetApiDocTests {
 
 	@Autowired
 	Pets pets;
+
+	@Autowired
+	ObjectMapper objectMapper;
 
 	@Test
 	public void getPets() throws Exception {
@@ -43,6 +51,24 @@ public class PetApiDocTests {
 		this.mvc.perform(get("/pets/{id}", pet.getId())) //
 			.andExpect(status().isOk()) //
 			.andDo(document("get-pet"));
+	}
+
+	@Test
+	public void updatePet() throws Exception {
+		Map<String, Object> createPet = new LinkedHashMap<>();
+		createPet.put("id", 1L);
+		createPet.put("name", "showdy");
+		createPet.put("type", Pet.PetType.ASSERTIVE);
+
+		this.mvc
+			.perform(post("/pets").contentType(MediaTypes.HAL_JSON)
+				.content(this.objectMapper.writeValueAsString(createPet)))
+			.andExpect(status().isCreated())
+			.andDo(document("create-pet", requestFields(//
+					fieldWithPath("id").description("The id of the Pet"), //
+					fieldWithPath("name").description("The name of the Pet"), //
+					fieldWithPath("type").description("The type of the Pet") //
+			)));
 	}
 
 }
